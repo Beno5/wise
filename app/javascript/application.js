4b64772f2cd9f00d09ccf7ba87f5adc37cd46ea4
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2025-01-15 23:30:44 +0100

    email ema fixes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "custom/svg-inject"
+import "custom/headhesive"
 import "custom/plugins"
 import "custom/theme"
+
--- app/javascript/custom/theme.js
+++ app/javascript/custom/theme.js
@@ -7,6 +7,7 @@ var theme = {
    * Do not forget to remove dependency from src/js/vendor/ and recompile.
    */
   init: function () {
+    theme.stickyHeader();
     theme.subMenu();
     theme.offCanvas();
     theme.isotope();
@@ -18,6 +19,7 @@ var theme = {
     theme.backgroundImageMobile();
     theme.imageHoverOverlay();
     theme.rellax();
+    theme.scrollCue();
     theme.swiperSlider();
     theme.lightbox();
     theme.plyr();
@@ -28,17 +30,38 @@ var theme = {
     theme.bsTooltips();
     theme.bsPopovers();
     theme.bsModal();
+    theme.iTooltip();
     theme.forms();
     theme.passVisibility();
     theme.pricingSwitcher();
     theme.textRotator();
+    theme.codeSnippet();
   },
   /**
    * Sticky Header
    * Enables sticky behavior on navbar on page scroll
    * Requires assets/js/vendor/headhesive.min.js
   */
-  
+  stickyHeader: () => {
+    var navbar = document.querySelector(".navbar");
+    if (navbar == null) return;
+    var options = {
+      offset: 350,
+      offsetSide: 'top',
+      classes: {
+        clone: 'navbar-clone fixed',
+        stick: 'navbar-stick',
+        unstick: 'navbar-unstick',
+      },
+      onStick: function() {
+        var navbarClonedClass = this.clonedElem.classList;
+        if (navbarClonedClass.contains('transparent') && navbarClonedClass.contains('navbar-dark')) {
+          this.clonedElem.className = this.clonedElem.className.replace("navbar-dark","navbar-light");
+        }
+      }
+    };
+    var banner = new Headhesive('.navbar', options);
+  },
   /**
    * Sub Menus
    * Enables multilevel dropdown
