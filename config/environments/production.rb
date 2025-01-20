9ef8f8f787badc267c93acd8ab8fb75e6a1eb50b
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2025-01-20 15:45:43 +0100

    domain bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,10 +46,10 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  #config.force_ssl = true
 
-  config.hosts << "wiseaustria.com"
-  config.hosts << "www.wiseaustria.com"
+  #config.hosts << "wiseaustria.com"
+  #config.hosts << "www.wiseaustria.com"
 
 
   # Include generic and useful information about system operation, but avoid logging too much
