a658a3fc9c5740991dc2f96206a5d5d09bcdf676
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2025-02-06 12:10:31 +0100

    text and pic

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,8 @@ class Project < ApplicationRecord
 
   validates :title, presence: true
   validates :content, presence: true
+
+  def plain_content
+    ActionView::Base.full_sanitizer.sanitize(content)
+  end
 end
