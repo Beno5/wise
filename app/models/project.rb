50c664fb67e7c7cbd90ace9ff7fb80ba6d2a6dd5
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2025-02-12 20:41:11 +0100

    cloudinary

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,21 @@ class Project < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  before_save :apply_cloudinary_transformation
+
+
   def plain_content
-    ActionView::Base.full_sanitizer.sanitize(content)
+      ActionView::Base.full_sanitizer.sanitize(content)
   end
+
+
+  def apply_cloudinary_transformation
+    return unless image.attached?
+
+    image.variant(
+      resize_to_fill: [950, 520, { crop: :pad, background: "white" }],
+      quality: "auto"
+    )
+  end
+
 end
